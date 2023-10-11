6ed5f8a6453ed37c8dde8d8f943fdfa99d9ed97c
CHANGWOO SON <96860725+5onchangwoo@users.noreply.github.com>
2023-10-11 16:08:02 +0900

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -29,8 +29,6 @@ markdown_text = f"""
   <img src="https://img.shields.io/badge/Gmail-EA4335?style=round-square&logo=Gmail&logoColor=white"/> <a> scw9410@gmail.com </a> <br/>
 #### Blog
   <img src="https://img.shields.io/badge/Tistory-000000?style=round-square&logo=Tistory&logoColor=white"/> <a>https://teching.tistory.com</a> <br/>
-#### Resume
-  <img src="https://img.shields.io/badge/Notion-000000?style=round-square&logo=Notion&logoColor=white"/> <a>https://sonchangwoo.notion.site</a> <br/>  
 
 <br/>
 <br/>
